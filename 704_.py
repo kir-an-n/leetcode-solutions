651940950c2f2167c1b54bd0f77a65037f4c3b5d
kir-an-n <207713602+kir-an-n@users.noreply.github.com>
2026-04-03 20:46:10 +0530

    added problem 704 solution

---
+++ 704_.py
@@ -0,0 +1,19 @@
+def search(nums,target):
+    left=0
+    right=len(nums)-1
+
+    while left <=right:
+        mid=(left+right) //2
+
+
+        if nums[mid] ==target:
+            return mid
+        elif nums[mid] < target:
+            left =mid+1
+        else:
+            right=mid-1
+
+    return -1
+print(search([1,3,5,7,9,1,13],7))
+
+
