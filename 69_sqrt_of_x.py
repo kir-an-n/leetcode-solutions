784bbfd63233e09c5d520d73f1ec813cea468e77
kir-an-n <207713602+kir-an-n@users.noreply.github.com>
2026-04-04 15:10:11 +0530

    added solution to leetcode problem 69

---
+++ 69_sqrt_of_x.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+       
+        low=1
+        high=x
+        ans=0
+
+        while low <=high :
+              mid=(low+high) //2
+        
+              if mid*mid==x:
+                 return mid
+
+              elif mid*mid < x:
+                   ans=mid
+                   low=mid+1
+
+              else:
+                   high=mid-1
+            
+        return ans
+             
+             
+
+
+        
