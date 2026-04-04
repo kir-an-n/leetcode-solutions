ec10d31fe8f97f8960515fc82d63f3de6221b168
kir-an-n <207713602+kir-an-n@users.noreply.github.com>
2026-04-04 14:19:18 +0530

    added solution to leetcode problem 74

---
+++ 74_2d_matrix_binary_search.py
@@ -0,0 +1,31 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+           return False
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+    
+        left=0
+        right=(rows*cols)-1
+    
+        while left <=right:
+              mid= (left + right) //2
+              r=mid // cols
+              c=mid % cols
+        
+              val=matrix[r][c]
+        
+              if val ==target:
+                 return True
+            
+              elif val< target:
+                   left=mid+1
+            
+              else:
+                   right=mid-1
+            
+        return False
+
+
+
